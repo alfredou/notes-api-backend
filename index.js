4a67bb385da4fe3f7dc7fd8c1c2c46f98d0f2fca
alfredo <alfredouiop@gmail.com>
2022-04-29 00:34:43 -0400

    Add heroku needed config

--- index.js
+++ index.js
@@ -77,7 +77,8 @@ app.use((request, response)=>{
      error: 'not found'
   })
 })
-const PORT = 3001
+//esta variable de entorno la necesita heroku para el poner y usar el puerto que quiera. 
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`); 
